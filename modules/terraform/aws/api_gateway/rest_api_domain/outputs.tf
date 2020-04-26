58dfb726cddba2045a819e7baf95499bff238229
Prasanna12510 <prasanna@hooq.tv>
2020-04-26 20:33:35 +0800

    added cloudfront domain_name and zone_id

--- modules/terraform/aws/api_gateway/rest_api_domain/outputs.tf
+++ modules/terraform/aws/api_gateway/rest_api_domain/outputs.tf
@@ -2,3 +2,11 @@ output domain_name {
   description = "Custom domain name."
   value       = "${aws_api_gateway_domain_name.domain.domain_name}"
 }
+
+output cloudfront_domain_name {
+  value = "${aws_api_gateway_domain_name.domain.cloudfront_domain_name}"
+}
+
+output cloudfront_zone_id {
+  value = "${aws_api_gateway_domain_name.domain.cloudfront_zone_id}"
+}
--- modules/terraform/aws/route53/recordsets/outputs.tf
+++ modules/terraform/aws/route53/recordsets/outputs.tf
@@ -2,15 +2,6 @@ output "dns_record_name" {
   value = var.is_A_record ? concat(aws_route53_record.a_type_record.*.name, [""])[0] : concat(aws_route53_record.not_a_type_record.*.name, [""])[0]
 }
 
-output "cloudfront_domain_name" {
-  value = var.is_A_record ? concat(aws_route53_record.a_type_record.*.cloudfront_domain_name, [""])[0] : ""
-}
-
-output "cloudfront_zone_id" {
-  value = var.is_A_record ? concat(aws_route53_record.a_type_record.*.cloudfront_zone_id, [""])[0] : ""
-}
-
-
 output "dns_record_value" {
-  value = var.is_A_record ? aws_route53_record.a_type_record.*.records : aws_route53_record.not_a_type_record.*.records
+  value = var.is_A_record ? aws_route53_record.a_type_record.*.records : aws_route53_record.a_type_record.*.records
 }

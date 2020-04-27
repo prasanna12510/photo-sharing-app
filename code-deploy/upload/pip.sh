98e3ee63c1d7f4996dd8f66caa3ec7ab3723f305
Prasanna12510 <prasanna@hooq.tv>
2020-04-28 01:00:59 +0800

    fix python library issue

--- code-deploy/upload/pip.sh
+++ code-deploy/upload/pip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd $1
-#pip install -r requirements.txt -t .
-python3 -m pip install --system -r requirements.txt -t $(pwd)
+pip install -r requirements.txt -t .
+#python3 -m pip install --system -r requirements.txt -t $(pwd)

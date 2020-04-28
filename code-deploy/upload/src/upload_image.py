1b54b17f999cc70fed4a0b3764f79a3b2be7a6bd
Prasanna12510 <prasanna@hooq.tv>
2020-04-28 16:55:49 +0800

    added code change in image upload

--- code-deploy/upload/src/upload_image.py
+++ code-deploy/upload/src/upload_image.py
@@ -17,17 +17,19 @@ def lambda_handler(event, context):
         logger.info('Event logging enabled: `{}`'.format(json.dumps(event)))
 
     if event['httpMethod'] == 'POST' :
-        data  = json.loads(event['body'])
-        name  = data['name']
-        image = data['file']
-        image_data = image[image.find(",")+1:]
-        content_type = image[image.find(":")+1:image.find(";")]
-        image_type = image[image.find("data:")+1:image.find(";")].split('/')[1]
+        #data  = json.loads(event['body'])
+        #name  = data['name']
+        #image = data['file']
+        #image_data = image[image.find(",")+1:]
+        #content_type = image[image.find(":")+1:image.find(";")]
+        #image_type = image[image.find("data:")+1:image.find(";")].split('/')[1]
 
         image_id = str(uuid.uuid4())[:8]
-        object_key = '{id}/{name}.{type}'.format(id=image_id, name=name,type=image_type)
-        dec = base64.b64decode(image_data + "===")
-        s3.put_object(Bucket=os.environ["BUCKET_NAME"], Key=object_key, Body=dec)
+        content_type = event["headers"]["content-type"]
+        image_type =  content_type.split('/')[1]
+        object_key = '{id}/{name}.{type}'.format(id=image_id, name=image_id,type=image_type)
+        dec = base64.b64decode(event['body'])
+        s3.put_object(Bucket=os.environ["BUCKET_NAME"], Key=object_key, Body=dec, ContentType=content_type)
 
         return {
         'statusCode': 200,

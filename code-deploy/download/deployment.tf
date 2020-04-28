784c7adefd15bdccc96316b742300e4d6c86ba37
Prasanna12510 <prasanna@hooq.tv>
2020-04-29 01:32:59 +0800

    added code chage to image download

--- code-deploy/download/deployment.tf
+++ code-deploy/download/deployment.tf
@@ -25,18 +25,11 @@ module  "download_api_resource" {
   path_parts             = ["download"]
 }
 
-module  "download_api_resource_id" {
+module  "download_api_resource_image_id" {
   source                 = "../../modules/terraform/aws/api_gateway/rest_api_resource"
   api_id                 = data.terraform_remote_state.photo_sharing_infra_state.outputs.api_id
   api_root_resource_id   = module.download_api_resource.resource_id
-  path_parts              = ["{id}"]
-}
-
-module  "download_api_resource_filename" {
-  source                 = "../../modules/terraform/aws/api_gateway/rest_api_resource"
-  api_id                 = data.terraform_remote_state.photo_sharing_infra_state.outputs.api_id
-  api_root_resource_id   = module.download_api_resource_id.resource_id
-  path_parts             = ["{filename}"]
+  path_parts              = ["{image_id}"]
 }
 
 module "download_image_api_method" {
@@ -46,8 +39,8 @@ module "download_image_api_method" {
   integration_type                = "AWS"
   http_method                     = "GET"
   bucket_name                     = data.terraform_remote_state.photo_sharing_infra_state.outputs.image_storage_s3_bucket_name
-  api_resource_id                 = module.download_api_resource_filename.resource_id
-  api_resource_path               = module.download_api_resource_filename.resource_path
+  api_resource_id                 = module.download_api_resource_image_id.resource_id
+  api_resource_path               = module.download_api_resource_image_id.resource_path
   request_parameters              = var.request_parameters
   integration_request_parameters  = var.integration_request_parameters
   credentials                     = data.terraform_remote_state.photo_sharing_infra_state.outputs.api_gateway_role_arn
@@ -56,8 +49,6 @@ module "download_image_api_method" {
 }
 
 ########################outputs###########################
-
-
 output "download_image_api_method_id" {
   value = module.download_image_api_method.http_method_id
 }
--- code-deploy/download/variables.tf
+++ code-deploy/download/variables.tf
@@ -2,8 +2,7 @@
 variable "request_parameters" {
   type = map
   default = {
-      "method.request.path.id"                    = true
-      "method.request.path.filename"              = true
+      "method.request.path.image_id"              = true
       "method.request.header.Content-Type"        = false
       "method.request.header.Content-Disposition" = false
 
@@ -14,8 +13,7 @@ variable "request_parameters" {
 variable "integration_request_parameters" {
   type = map
   default = {
-      "integration.request.path.id"       = "method.request.path.id"
-      "integration.request.path.filename" = "method.request.path.filename"
+      "integration.request.path.image_id"       = "method.request.path.image_id"
       "integration.request.header.Content-Type" = "method.request.header.Content-Type"
       "integration.request.header.Content-Disposition" = "method.request.header.Content-Disposition"
     }
